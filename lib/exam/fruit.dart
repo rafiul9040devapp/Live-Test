f0edc5e210bd2c24bcdb4dbbac08f2690258f2ff
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2023-12-12 00:03:55 +0600

    live test is done

--- lib/exam/fruit.dart
+++ lib/exam/fruit.dart
@@ -16,26 +16,6 @@ class Fruit {
   }
 }
 
-void main() {
-  Fruit fruit1 = Fruit("Apple", "Red", 2.5);
-  Fruit fruit2 = Fruit("Banana", "Yellow", 1.0);
-  Fruit fruit3 = Fruit("Grapes", "Purple", 3.0);
-
-  Map<int, Fruit> mapOfFruits = {
-    1: fruit1,
-    2: fruit2,
-    3: fruit3,
-  };
-
-  List<Fruit> listOfFruits() =>
-      mapOfFruits.entries.map((fruit) => fruit.value).toList();
-
-  print("Original Fruit Details before Discount:");
-  displayFruitDetails(listOfFruits());
-  print("\n------------------------\n");
-  applyPriceDiscount(listOfFruits(), 10);
-}
-
 void displayFruitDetails(List<Fruit>? fruits) {
   if (fruits != null && fruits.isNotEmpty) {
     for (var fruit in fruits) {
